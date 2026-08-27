2acc39d52bbd810f5d610a83d66d1d82016b7b26
Lochlin Dormer <lochiedormer@hotmail.com>
2026-08-27 16:40:43 +1000

    Make Next Gigs link green instead of white

--- src/app/(public)/PromoLinks.tsx
+++ src/app/(public)/PromoLinks.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 export default function PromoLinks({ showBooking = true }: { showBooking?: boolean }) {
   return (
-    <div className="flex gap-3 text-[9px] font-semibold uppercase leading-snug text-center text-foreground">
-      <Link href="/gigs" className="hover:opacity-80">
+    <div className="flex gap-3 text-[9px] font-semibold uppercase leading-snug text-center">
+      <Link href="/gigs" className="text-success hover:opacity-80">
         Next
         <br />
         Gigs
       </Link>
       {showBooking && (
-        <Link href="/book" className="hover:opacity-80">
+        <Link href="/book" className="text-foreground hover:opacity-80">
           Book
           <br />
           Lochie
